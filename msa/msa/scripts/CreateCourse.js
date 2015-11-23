c89a0b0a2c1c527bc9cd6bb078de2fc926202d2a
Shinobu Williams <swi67@uclive.ac.nz>
2015-11-24 00:27:03 +1300

    Creation implemented

---
+++ msa/msa/scripts/CreateCourse.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    setupCourseSubmit();
+    setupReturn();
+});
+
+function setupCourseSubmit() {
+
+    //Creating student from form parameters
+
+    var form = document.forms.create;
+    // Need to add our own custom event for form submission
+    form.onsubmit = function (e) {
+        // ... and prevent the default action from occuring
+        e.preventDefault();
+
+        //Creating student from form parameters
+        var newCourse = {
+            // Access the data in the fields with .value 
+            title: document.getElementById("Titleinput").value,
+            credits: document.getElementById("Creditsinput").value,     
+        }
+
+        // Take me back home when done!
+        CourseModule.addCourse(newCourse, function () {
+            window.location.href = "course.html";
+        });
+    }
+
+};
+
+// Add event listener, cancel button will take you back to home page
+function setupReturn() {
+    document.getElementById('btncancel').addEventListener('click', function () {
+        window.location.href = "course.html";
+    });
+}
